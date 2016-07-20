28293e9bb28230305985335c91ecbe5541c3361d
Irakli Lekishvili <l3kishvili@gmail.com>
2016-07-20 17:13:16 +0400

    Remindrr#configure specs

--- spec/remindrr/remindrr_spec.rb
+++ spec/remindrr/remindrr_spec.rb
@@ -4,4 +4,14 @@ describe Remindrr do
   it 'has a version number' do
     expect(Remindrr::VERSION).not_to be nil
   end
+
+  describe '#configure' do
+    it 'sets correct configuration' do
+      Remindrr.configure do |config|
+        config.client_secret = 'client_secret'
+      end
+
+      expect(Remindrr.config.client_secret).to eql('client_secret')
+    end
+  end
 end
