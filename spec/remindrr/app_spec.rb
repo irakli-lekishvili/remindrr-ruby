dc512b16f668b8aec57e4673cf8bd52eede31381
Irakli Lekishvili <l3kishvili@gmail.com>
2016-07-20 20:15:48 +0400

    added vcr development dependency

--- spec/remindrr/app_spec.rb
+++ spec/remindrr/app_spec.rb
@@ -1,20 +1,39 @@
 require 'spec_helper'
 
 describe Remindrr::App do
+  before do
+    Remindrr.configure do |config|
+      config.client_secret = '12345'
+    end
+  end
 
   context 'attributes' do
     describe Remindrr::App.new({
-        id: 1,
-        name: 'name',
-        endpoint: 'http://localhost:3000',
-        created_at: Time.new(1),
-        updated_at: Time.new(2)
-      }) do
+      id: 1,
+      name: 'name',
+      endpoint: 'http://localhost:3000',
+      created_at: Time.new(1),
+      updated_at: Time.new(2)
+    }) do
 
       it { is_expected.to have_attributes(id: 1, name: 'name') }
       it { is_expected.to have_attributes(endpoint: 'http://localhost:3000') }
       it { is_expected.to have_attributes(created_at: Time.new(1)) }
       it { is_expected.to have_attributes(updated_at: Time.new(2)) }
+    end
+  end
+
+  context '#all' do
+    # before { @apps = Remindrr::App.all }
+    before do
+      stub_get("v1/apps").
+        with(headers: {'Authorization': 'Token #{Remindrr.config.client_secret}'}).
+      to_return(status: 200, body: "")
+    end
+
+    it do
+      @apps = Remindrr::App.all
+      binding.pry
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
-require 'remindrr'
 require 'rspec'
 require 'webmock/rspec'
+require 'remindrr'
 
-RSpec.configure do |config|
-  config.include WebMock::API
-end
-
-def fixture_path
-  File.expand_path('../fixtures', __FILE__)
-end
-
-def fixture(file)
-  File.new(fixture_path + '/' + file)
+VCR.configure do |vcr|
+  vcr.cassette_library_dir = "spec/fixtures/cassettes"
+  vcr.hook_into :webmock
 end
