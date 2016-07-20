d8252fb8516c9efaad0e364b3b83ea1e0b0e191e
Irakli Lekishvili <l3kishvili@gmail.com>
2016-07-20 17:11:00 +0400

    added configuration

--- lib/remindrr.rb
+++ lib/remindrr.rb
@@ -2,6 +2,7 @@ require 'pry-byebug'
 require 'faraday'
 
 require 'remindrr/version'
+require 'remindrr/configuration'
 require 'remindrr/error'
 require 'remindrr/connection'
 require 'remindrr/request'
@@ -10,4 +11,19 @@ require 'remindrr/app'
 require 'remindrr/reminder'
 
 module Remindrr
+  def self.configure
+    yield config
+  end
+
+  def self.config
+    @config ||= Configuration.new
+  end
+
+  def self.config=(config)
+    @config = config
+  end
+
+  configure do |config|
+    config.client_secret = ENV['CLIENT_SECRET']
+  end
 end
---
+++ lib/remindrr/configuration.rb
@@ -0,0 +1,5 @@
+module Remindrr
+  class Configuration
+    attr_accessor :client_secret
+  end
+end
